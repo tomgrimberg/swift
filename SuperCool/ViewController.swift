0a834a049f00313c3c144896b59b6eeb823243aa
Tom Grim <tomgrim@Toms-Mac-mini.local>
2016-07-11 19:49:47 +0300

    add supercool

--- SuperCool/ViewController.swift
+++ SuperCool/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var luluLogo: UIImageView!
+    @IBOutlet weak var coolbg: UIImageView!
+    @IBOutlet weak var unCoolButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +24,12 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func MakeMeCool(sender: AnyObject) {
+        luluLogo.hidden=false
+        coolbg.hidden=false
+        unCoolButton.hidden=true
+        
+    }
 
 }
 
